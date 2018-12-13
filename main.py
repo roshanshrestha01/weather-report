5eb3c03a184de2954192b2dd8623fd1585a060f8
Roshan Shrestha <roshanshrestha01@gmail.com>
2018-12-13 10:09:26 +0545

    helper function to add and remove box from main window and check for empty city name to fetch api

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import gi
 import requests
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+from settings import WEATHER_BASE_API, API_KEY
 
 
-class WeatherForecaseWindow(Gtk.Window):
+class WeatherForecastWindow(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="ListBox Demo")
+        Gtk.Window.__init__(self, title="Weather forecast")
         self.set_position(Gtk.WindowPosition.CENTER)
         self.set_border_width(10)
+        self.CITY_NAME = None  # for refresh action
+        self.add_to_main(self.setup_city_name_box, 'city_name_gui')
 
+    def setup_city_name_box(self):
         box_outer = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(box_outer)
-
         listbox = Gtk.ListBox()
         listbox.set_selection_mode(Gtk.SelectionMode.NONE)
         box_outer.pack_start(listbox, True, True, 0)
@@ -27,7 +30,7 @@ class WeatherForecaseWindow(Gtk.Window):
         city_name = Gtk.Label("City name", xalign=0)
         self.city_name = Gtk.Entry()
         self.city_name.set_size_request(400, 20)
-        self.city_name.set_text("Kathmandu")
+        # self.city_name.set_text("Kathmandu")
         self.city_name.set_width_chars(40)
 
         vbox.pack_start(city_name, False, False, 0)
@@ -44,17 +47,43 @@ class WeatherForecaseWindow(Gtk.Window):
 
         self.update = Gtk.Button.new_with_label("Refresh")
         self.update.set_size_request(200, 20)
-        hbox.pack_start(self.check_weather, False, False, 0)
-        hbox.pack_start(self.update, False, False, 0)
+        hbox.pack_end(self.check_weather, False, False, 0)
+        # hbox.pack_start(self.update, False, False, 0)
 
         listbox.add(row)
+        return box_outer
+
+    def add_to_main(self, box, key):
+        box = box()
+        setattr(self, key, box)
+        self.add(getattr(self, key))
+
+    def remove_from_main(self, key):
+        self.remove(getattr(self, key))
 
     def get_weather_forecast(self, button):
         city_name = self.city_name.get_text()
+        if not city_name:
+            dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO,
+                                       Gtk.ButtonsType.OK, "Please enter a city name.")
+            dialog.format_secondary_text(
+                "You have place empty city name.")
+            dialog.run()
+            print("INFO dialog closed")
+
+            dialog.destroy()
+            return
+        self.remove_from_main('city_name_gui')
+        self.CITY_NAME = city_name
+
+        url = WEATHER_BASE_API + city_name + '&appid=' + API_KEY
+        response = requests.get(url)
+
+        print(response.json())
         print('Getting weather report from google for %s' % self.city_name.get_text())
 
 
-win = WeatherForecaseWindow()
+win = WeatherForecastWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
---
+++ settings.py
@@ -0,0 +1,3 @@
+API_KEY = 'b6907d289e10d714a6e88b30761fae22'
+
+WEATHER_BASE_API = 'https://samples.openweathermap.org/data/2.5/forecast?q='
